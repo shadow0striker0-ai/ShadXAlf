745c26f41556f348f19dfafc4b12d719cc9cb9db
shadow <shadow0striker0@gmail.com>
2026-09-16 18:16:31 +0200

    fixed credits

--- SOURCE/MODULES/CREDITS/show_credits.py
+++ SOURCE/MODULES/CREDITS/show_credits.py
@@ -42,7 +42,7 @@ BANNER = r"""
                 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                  \
                   \__________________________________________________________________________
-                   \ Credits go to @Alfredbird who helped with creating this Framework       |
+                   \ Credits go to @Alfredredbird who helped with creating this Framework       |
                     \     checkout his github: https://github.com/Alfredredbird              |
                      \_______________________________________________________________________|
 """
